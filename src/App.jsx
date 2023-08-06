74f1fecee36170d203c4e41212e916847640aaee
blackgenius <zohoungbogboanlyou@gmail.com>
2023-08-07 00:52:01 +0100

    assets add in router create

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
+import { routes } from "./routes/router";
 
 function App() {
   return (
-    <main className="flex justify-center gap-4 flex-col min-h-screen">
-      <h1 className="text-3xl text-center font-bold underline">React & Tailwind CSS Starter Pack</h1>
-      <p className="text-center text-xl">This is a starter pack for React & Tailwind CSS projects.</p>
-      <img src="https://bit.ly/3wsmzTy" alt="meme" className="mx-auto" />
-    </main>
+    <RouterProvider router={routes}/>
   );
 }
 
