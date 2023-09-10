264ef2556a9462461f6535f383986afeaa89c387
d4rcel <daraola05@gmail.com>
2023-09-10 23:47:54 +0100

    Building mobile version Discover

--- src/components/artist/Description.jsx
+++ src/components/artist/Description.jsx
@@ -3,12 +3,12 @@ import description from '../../assets/images/discoverDescription.svg';
 
 function Description() {
     return (
-        <div className='flex mb-20'>
-            <div className='w-1/2 bg-tertiary flex flex-col justify-center items-center gap-y-6'>
-                <h2 className='text-3xl font-bold'>Welcome to the art and history</h2>
-                <p className='text-base text-center w-[35rem]'>Lorem ipsum dolor sit amet consectetur. Eu habitant nec arcu massa aenean lectus tristique posuere quam. Venenatis odio proin amet suspendisse enim. Tincidunt suspendisse id quis metus dapibus pulvinar. Mauris vulputate maecenas convallis ut amet pellentesque elit. Nibh viverra semper arcu sed morbi dolor aliquam elit. Pharetra vitae scelerisque turpis cursus a natoque orci tortor amet. Viverra convallis enim pulvinar fringilla posuere nunc euismod. Proin mauris sit enim pellentesque eu lorem. Quis facilisis molestie ornare in vestibulum pulvinar et ut ornare.</p>
+        <div className='flex flex-col mb-20  md:flex-row'>
+            <div className='bg-tertiary flex flex-col justify-center items-center gap-y-6 py-6 md:w-1/2'>
+                <h2 className='text-3xl text-center font-bold'>Welcome to the art and history</h2>
+                <p className='text-center w-full'>Lorem ipsum dolor sit amet consectetur. Eu habitant nec arcu massa aenean lectus tristique posuere quam. Venenatis odio proin amet suspendisse enim. Tincidunt suspendisse id quis metus dapibus pulvinar. Mauris vulputate maecenas convallis ut amet pellentesque elit. Nibh viverra semper arcu sed morbi dolor aliquam elit. Pharetra vitae scelerisque turpis cursus a natoque orci tortor amet. Viverra convallis enim pulvinar fringilla posuere nunc euismod. Proin mauris sit enim pellentesque eu lorem. Quis facilisis molestie ornare in vestibulum pulvinar et ut ornare.</p>
             </div>
-            <div className='w-1/2'>
+            <div className='md:w-1/2'>
                 <img src={description} alt="" />
 
             </div>
--- src/components/home/Exposure.jsx
+++ src/components/home/Exposure.jsx
@@ -7,21 +7,21 @@ import ExposureDescription from './ExposureDescription'
 function Exposure() {
     return (
         <div className='flex flex-col'>
-            <div className='flex items-center flex-col gap-16 max-w-6xl2 mb-16 mx-5 md:mx-auto md:flex-row md:py-12 md:px-8 md:mb-0'>
+            <div className='flex items-center flex-col gap-16 max-w-6xl2 mb-16 mx-5 md:mx-auto md:flex-row md:py-12 md:px-4 md:mb-0'>
                 <ExposureDescription />
 
                 <div className='md:grow md:shrink md:basis-default -order-1 md:order-1'><img src={firstExpo} alt="" className='w-full' /></div>
             </div>
 
 
-            <div className='flex items-center flex-col gap-16 max-w-6xl2 mb-16 my-0 mx-5 md:mx-auto md:flex-row md:py-12 md:px-8 md:mb-0'>
+            <div className='flex items-center flex-col gap-16 max-w-6xl2 mb-16 my-0 mx-5 md:mx-auto md:flex-row md:py-12 md:px-4 md:mb-0'>
                 <div className='md:grow md:shrink md:basis-default'><img src={secondExpo} alt="" className='w-full' /></div>
                 <ExposureDescription />
             </div>
 
 
 
-            <div className='flex items-center flex-col gap-16 max-w-6xl2 mb-16 mx-5 md:mx-auto md:flex-row md:py-12 md:px-8 md:mb-0'>
+            <div className='flex items-center flex-col gap-16 max-w-6xl2 mb-16 mx-5 md:mx-auto md:flex-row md:py-12 md:px-4 md:mb-0'>
                 <ExposureDescription />
 
                 <div className='md:grow md:shrink md:basis-default -order-1 md:order-1'><img src={thirdExpo} alt="" className='w-full' /></div>
